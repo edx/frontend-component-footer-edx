fdafb0bcfeb2cc452dcc9b8259f13f4ac24a2446
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2022-11-28 13:57:49 +0500

    Update frontend-build to v12 (#196)
    * refactor: updated frontend-build to v12 & resolved eslint issues

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default, EVENT_NAMES } from './components/Footer';
+export { default as Footer, EVENT_NAMES } from './components/Footer';
 export { default as messages } from './i18n/index';
