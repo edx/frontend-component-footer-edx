8ad94d1edd0aca06223e108ddb7ac94664fc136f
Leangseu Kim <lkim@edx.org>
2022-12-12 15:09:05 -0500

    fix: make footer export backward compat

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default as Footer, EVENT_NAMES } from './components/Footer';
+export { default, default as Footer, EVENT_NAMES } from './components/Footer'; // eslint-disable-line no-restricted-exports
 export { default as messages } from './i18n/index';
