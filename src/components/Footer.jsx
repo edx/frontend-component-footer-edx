1ac2721b251a6eb322d204dc8882cff88c90393a
Muhammad Abdullah Waheed <42172960+abdullahwaheed@users.noreply.github.com>
2023-01-30 12:14:26 +0500

    feat: hide app download buttons when page accessed from edx mobile apps (#248)
    * feat: hide app download buttons when page accessed from edx mobile apps
    
    * fix: linting issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -67,6 +67,9 @@ class Footer extends React.Component {
     return `/${twoLetterPrefix}`;
   }
 
+  // Added specific check for edX mobile app
+  isMobile = () => navigator.userAgent.match('org.edx.mobile');
+
   externalLinkClickHandler(event) {
     const label = event.currentTarget.getAttribute('href');
     const eventName = EVENT_NAMES.FOOTER_LINK;
@@ -233,6 +236,7 @@ class Footer extends React.Component {
             <ul className="d-flex flex-row justify-content-between list-unstyled max-width-222 p-0 mb-4">
               <SocialIconLinks onClick={this.externalLinkClickHandler} />
             </ul>
+            {!this.isMobile() && (
             <ul className="d-flex flex-row justify-content-between list-unstyled max-width-264 p-0 mb-5">
               <li>
                 <GooglePlayStoreButton onClick={this.externalLinkClickHandler} />
@@ -241,6 +245,7 @@ class Footer extends React.Component {
                 <AppleAppStoreButton onClick={this.externalLinkClickHandler} />
               </li>
             </ul>
+            )}
             <p>
               © {new Date().getFullYear()} edX LLC. All rights reserved.
               <br />
