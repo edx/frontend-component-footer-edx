49751e227da2406e2d3383010f6e846746776b11
Leangseu Kim <lkim@edx.org>
2023-03-03 12:12:37 -0500

    fix: add default export for edx footer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-export { default as Footer, EVENT_NAMES } from './components/Footer';
-export { default as messages } from './i18n/index';
+import Footer, { EVENT_NAMES } from './components/Footer';
+import messages from './i18n/index';
+
+// preserving sub export footer to match with all of v5 release.
+export { Footer, messages, EVENT_NAMES };
+export default Footer;
