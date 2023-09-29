2e2d055c46117455da8c374fed5a8d916c6ebd15
KristinAoki <kaoki@2u.com>
2023-09-29 15:45:24 -0700

    feat: add studio footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Footer, { EVENT_NAMES } from './components/Footer';
+import StudioFooter from './components/studio-footer';
 import messages from './i18n/index';
 
 // preserving sub export footer to match with all of v5 release.
-export { Footer, messages, EVENT_NAMES };
+export { Footer, messages, EVENT_NAMES, StudioFooter };
 export default Footer;
