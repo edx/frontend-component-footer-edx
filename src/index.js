3cdcb7853f71778ca6650f028ea2c32b797f3c21
Maxwell Frank <mfrank@2u.com>
2025-04-24 17:31:20 +0000

    feat: add FooterSlot and StudioFooterSlot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Footer, { EVENT_NAMES } from './components/Footer';
 import StudioFooter from './components/studio-footer';
+import FooterSlot from './plugin-slots/FooterSlot';
+import StudioFooterSlot from './plugin-slots/StudioFooterSlot';
 import messages from './i18n/index';
 
 // preserving sub export footer to match with all of v5 release.
@@ -8,5 +10,7 @@ export {
   messages,
   EVENT_NAMES,
   StudioFooter,
+  FooterSlot,
+  StudioFooterSlot,
 };
 export default Footer;
