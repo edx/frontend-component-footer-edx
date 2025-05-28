5e7493e616c0b18dac26b52525194479c0d26780
Maxwell Frank <mfrank@2u.com>
2025-05-28 14:05:27 +0000

    fix: peerdeps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// const { createConfig } = require('@openedx/frontend-build');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { createConfig } = require('@openedx/frontend-build');
 
-// module.exports = createConfig('eslint');
+module.exports = createConfig('eslint');
